ed5ab921622fd13cc1d74636620879a377035e01
NinjaTrench <info@securitylabs.in>
2015-07-22 22:04:50 +0530

    clean rst file

--- setup.py
+++ setup.py
@@ -16,7 +16,6 @@ setup(
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
-        'Topic :: API for go.debian.net :: API client',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
@@ -25,6 +24,7 @@ setup(
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Libraries :: Python Modules'
     ],
 
     install_requires=['requests'],
