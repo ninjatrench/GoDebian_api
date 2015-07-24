f871b2b45b2f6dd5c9469f46fabd51138c11b4b2
Bernd Zeimetz <bernd@bzed.de>
2015-07-24 11:25:30 +0200

    Rewrite API calls as Python wrap function.

--- GoDebian/api.py
+++ GoDebian/api.py
@@ -8,6 +8,7 @@ except ImportError:
     print("requests and json libraries are required, but not found.")
     exit(1)
 
+from functools import wraps
 
 class ApiError(Exception):
     pass
@@ -30,24 +31,34 @@ class GoDebianApi(object):
         self.headers = {'Content-type': 'application/json'}
         self.check_ip_white_list()
 
+    def _api_call(func):
+        @wraps(func)
+        def _tmp(self, *args, **kwargs):
+            function_name = func.__name__
+
+            data = {'method': function_name, 'params': args, 'id': "jsonrpc"}
+            r = requests.post(self.api_url, headers=self.headers, data=json.dumps(data))
+            #print(r.status_code)
+
+            if r.status_code == 200:
+                resp = r.json()
+                if resp.get('result', False):
+                    return self.host + resp.get('result')
+                else:
+                    raise ApiError(resp.get('error', "Some error occurred"))
+            else:
+                raise ApiError("May be your host is not whitelisted in the api, visit https://wiki.debian.org/deb.li for more details.")
+        return _tmp
+            
+
+    @_api_call
     def add_url(self, url):
         """
         :param url: Provides shortened link for given URL
                     repeated URLs don't get different Keys.
         :return str: shortened URL
         """
-        data = {'method': "add_url", 'params': [url], 'id': "jsonrpc"}
-        r = requests.post(self.api_url, headers=self.headers, data=json.dumps(data))
-        #print(r.status_code)
-
-        if r.status_code == 200:
-            resp = r.json()
-            if resp.get('result', False):
-                return self.host + resp.get('result')
-            else:
-                raise ApiError(resp.get('error', "Some error occurred"))
-        else:
-            raise ApiError("May be your host is not whitelisted in the api, visit https://wiki.debian.org/deb.li for more details.")
+        pass
 
     def get_preview_url(self, key):
         """
@@ -56,43 +67,38 @@ class GoDebianApi(object):
         """
         return self.preview % key
 
+    @_api_call
     def get_url(self, key):
         """
         :param key: Enter the key to get associated URL
         Get key from following format : http://deb.li/p/<key>
         :return str: URL associated
         """
-        data = {'method': "get_url", 'params': [key], 'id': "jsonrpc"}
-        r = requests.post(self.api_url, headers=self.headers, data=json.dumps(data))
-        resp = r.json()
-        if resp.get('result', False):
-            return resp.get('result')
-        else:
-            raise ApiError("Some error occurred with given input: %s" % key)
+        pass
 
+    @_api_call
     def add_static_url(self, url, keyword):
         """
-
         :param url: Url to be shortened
         :param keyword: Static keyword against which url needs to be stored
         example : go.debian.net/<keyword>
         :return:
         """
+        pass
 
-        data = {'method': "add_static_url", 'params':[url, keyword], 'id': "jsonrpc"}
-        r = requests.post(self.api_url, headers=self.headers, data=json.dumps(data))
-        resp = r.json()
-        if resp.get('result', False):
-            return self.host + resp.get('result')
-        else:
-            raise ApiError(resp.get('error', "Some error occurred"))
-
-    def update_static_url(self, url):
-        print("implementation pending")
+    @_api_call
+    def update_static_url(self, url, keyword):
+        """
+        :param url: new url
+        :param keyword: Static keyword against which url needs to be stored
+        example : go.debian.net/<keyword>
+        :return:
+        """
 
     def check_ip_white_list(self):
         try:
-            _ = self.add_url("http://www.debian.org")
+        _ = self.add_url("http://www.debian.org")
         except Exception as e:
             print("Exception occured")
             print(e)
+
